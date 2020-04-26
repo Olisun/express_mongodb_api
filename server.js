8339719fe064fdb5a85e9ff9e14b3edbddaaa039
Oliver Sun <olisun@mac.com>
2020-04-26 13:19:54 -0700

    added packages body-parser, nodemon and mongodb. added .gitignore. initialized server.

---
+++ server.js
@@ -0,0 +1,13 @@
+// requiring dependencies 
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+// initializing express 
+const app = express();
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`We are live on ${port}`)
+});
